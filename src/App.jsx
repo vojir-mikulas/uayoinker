b67d653cd78ab399aa987d67ae3c2c1afefa8f9b
Mikuláš Vojíř <mikulas.vojir@r2b2.cz>
2023-12-11 15:38:08 +0100

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
+import {useRef, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {toast, Toaster} from "react-hot-toast";
 
 function App() {
-  const [count, setCount] = useState(0)
 
+    const handleCopyText = (e) =>{
+
+        const userAgentEl = e.currentTarget;
+
+        navigator.clipboard.writeText(userAgentEl.textContent);
+
+        toast.success('Copied to clipboard.')
+    }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Toaster/>
+    <h1>Your userAgent is:</h1>
+        <span onClick={(e)=>handleCopyText(e)}>{navigator.userAgent}</span>
     </>
   )
 }
